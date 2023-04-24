c7470919ee391dc0bdbf82adf31fadc66872d6cb
Alex Díaz <alejandro.dz.zpt@gmail.com>
2023-04-24 20:34:41 +0200

    To-Watch-Back - Primer commit.

---
+++ to-watch-back/src/main/java/com/imf/alumnos/daw/tfg/alexdiaz/towatchback/ToWatchBackApplication.java
@@ -0,0 +1,13 @@
+package com.imf.alumnos.daw.tfg.alexdiaz.towatchback;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ToWatchBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ToWatchBackApplication.class, args);
+	}
+
+}
---
+++ to-watch-back/src/test/java/com/imf/alumnos/daw/tfg/alexdiaz/towatchback/ToWatchBackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.imf.alumnos.daw.tfg.alexdiaz.towatchback;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ToWatchBackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
